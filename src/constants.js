33b2f5279d13321d8602593798c8170b3af9c6db
Utrennik <utrennik@list.ru>
2021-11-23 22:59:52 +0300

    feat: add url parsing, validation and basic CRUD methods logic branches

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+module.exports = {
+	methods: {
+		GET: 'GET',
+		POST: 'POST',
+		PUT: 'PUT',
+		DELETE: 'DELETE',
+	},
+
+	errorCodes: {
+		NOT_FOUND: 400,
+		INTERNAL_SERVER_ERROR: 500,
+	},
+
+	URLs: {
+		person: '/person',
+	},
+
+	categories: {
+		person: 'person',
+	},
+};
---
+++ src/index.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const http = require('http');
+const uuid = require('uuid');
+const constants = require('./constants');
+const NotFoundError = require('./errors/not-found-error');
+
+const server = http.createServer(async (req, res) => {
+	try {
+		const { method, url } = req;
+		const { methods } = constants;
+		const splittedURL = url.split('/');
+		const category = splittedURL[1];
+		const unvalidatedID = splittedURL[2];
+		const id = uuid.validate(unvalidatedID) ? unvalidatedID : null;
+
+		if (
+			(!id && splittedURL.length > 2) ||
+			(id && splittedURL.length > 3) ||
+			category !== constants.categories.person
+		) {
+			throw new NotFoundError();
+		}
+
+		if (method === methods.GET) {
+			res.writeHead(200, { 'Content-Type': 'application/json' });
+			res.write('Get Person(s)');
+		} else if (method === methods.POST && id) {
+			res.writeHead(200, { 'Content-Type': 'application/json' });
+			res.write('Post Person(s)');
+		} else if (method === methods.PUT && id) {
+			res.writeHead(200, { 'Content-Type': 'application/json' });
+			res.write('Put Person');
+		} else if (method === methods.DELETE && id) {
+			res.writeHead(200, { 'Content-Type': 'application/json' });
+			res.write('Delete Person');
+		} else {
+			throw new NotFoundError();
+		}
+
+		res.end();
+	} catch (e) {
+		const errorCode = e.errorCode || constants.errorCodes.INTERNAL_SERVER_ERROR;
+
+		res.writeHead(errorCode, { 'Content-type': 'application/json' });
+		res.end(JSON.stringify({ message: e.message }));
+	}
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+	console.log('\x1b[33m%s\x1b[0m', `Server started on port: ${PORT}`);
+});
